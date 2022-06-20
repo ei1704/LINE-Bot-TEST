3b4167922f3fb49cb19f769b7594682b00041812
Ryopey <ryopey1231@gmail.com>
2022-06-20 16:05:26 +0900

    make it better

---
+++ index.js
@@ -0,0 +1,58 @@
+const line = require('@line/bot-sdk');
+const express = require('express');
+
+// 環境変数からチャネルアクセストークンとチャネルシークレットを取得する
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET
+};
+// LINE クライアントを生成する : `channelSecret` が未定義だと例外が投げられる
+const client = new line.Client(config);
+
+// Express アプリを生成する
+const app = express();
+
+// LINE Bot SDK が提供するミドルウェアを挟み込み、リクエストヘッダの署名検証や JSON パースなどを任せてしまう
+app.post('/callback', line.middleware(config), (req, res) => {
+  // 1回のリクエストに複数のメッセージが含まれていたりすることもあるので
+  // イベントの配列を1件ずつ取得して処理してやる
+  const events = req.body.events;
+  Promise.all(events.map((event) => {
+    // イベント1件を処理する・エラー時も例外を伝播しないようにしておく
+    return handleEvent(event).catch(() => { return null; });
+  })
+    .then((result) => {
+      // 全てのイベントの処理が終わったら LINE API サーバには 200 を返す
+      res.status(200).json({}).end();
+    }))
+});
+
+/**
+ * イベント1件を処理する
+ * 
+ * @param {*} event イベント
+ * @return {Promise} テキストメッセージイベントの場合は client.pushMessage() の結果、それ以外は null
+ */
+function handleEvent(event) {
+  // メッセージイベントではない場合、テキスト以外のメッセージの場合は何も処理しない
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+
+  // 返信用メッセージを組み立てる : ユーザからのメッセージにカギカッコを付けて返信してみる
+  const echoMessage = {
+    type: 'text',
+    text: `「${event.message.text}」`
+  };
+
+  // Reply API を利用してリプライする
+  return client.replyMessage(event.replyToken, echoMessage);
+  // Push API を利用する場合は以下のようにする
+  // return client.pushMessage(event.source.userId, echoMessage);
+}
+
+// サーバを起動する
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
