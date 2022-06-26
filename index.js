dca55aaa014186a77fbf34c44f70c5cb952e00a4
Ryopey <ryopey1231@gmail.com>
2022-06-26 17:17:59 +0900

    test

--- index.js
+++ index.js
@@ -113,8 +113,9 @@ async function handleEvent(event) {
       userDatas[event.source.userId].password = event.message.text;
       console.log("login now");
       var temp = userDatas[event.source.userId].email;
-      const auth = getAuth();
-      await firebaseAuth.signInWithEmailAndPassword(temp, event.message.text)
+      //const auth = getAuth();
+      //await firebaseAuth.signInWithEmailAndPassword(temp, event.message.text)
+      await firebaseAuth.signInWithEmailAndPassword(userDatas[event.source.userId].email, event.message.text)
         .then((userCredential) => {
           console.log("login OK");
           // Signed in
